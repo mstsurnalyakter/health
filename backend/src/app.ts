6526151c06600641ec5dc154c85e4af9ff8fb6aa
mstsurnalyakter <surnalyms@gmail.com>
2025-07-20 14:12:04 +0600

    Add bcrypt for password hashing and update user/admin models with default values

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,19 +1,19 @@
 import express, { Application, Request, Response } from "express";
-import cors from "cors"
+import cors from "cors";
 import { userRoutes } from "./app/modules/User/user.routes";
 
-
-const app:Application = express()
+const app: Application = express();
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/",(req:Request,res:Response)=>{
-    res.send({
-        message:"Health server running"
-    })
-})
+app.get("/", (req: Request, res: Response) => {
+  res.send({
+    message: "Health server running",
+  });
+});
 
-app.use("/api/v1/user",userRoutes)
-
+app.use("/api/v1/user", userRoutes);
 
 export default app;
--- backend/src/app/modules/User/user.controller.ts
+++ backend/src/app/modules/User/user.controller.ts
@@ -2,11 +2,12 @@ import { Request, Response } from "express";
 import { userService } from "./user.service";
 
 const createAdmin = async (req: Request, res: Response) => {
-    const result = await userService.createAdmin();
+  const result = await userService.createAdmin(req.body);
+  console.log(result);
 
-    res.send(result)
+  res.send(result);
 };
 
 export const userController = {
-    createAdmin
-}
+  createAdmin,
+};
