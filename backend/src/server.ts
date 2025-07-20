9b727b8069b0f37f69eb6dc292a61b05492ed762
mstsurnalyakter <surnalyms@gmail.com>
2025-07-20 09:08:28 +0600

    Add initial backend setup with Prisma models and Express server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,14 @@
+import { Server } from "http";
+import app from "./app"
 
-import express from "express";
-const app = express()
+
 
 const PORT = 3000;
 
+const main = async () =>{
+    const server:Server = app.listen(PORT,()=>{
+        console.log(`Health server is running http://localhost:${PORT}`)
+    })
+}
 
-app.listen(PORT,()=>{
-    console.log(`App is listening at http://localhost:${PORT}`)
-})
+main()
